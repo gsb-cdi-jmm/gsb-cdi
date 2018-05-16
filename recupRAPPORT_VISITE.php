7376a8f261ff796bea9e88f96f3b5087c3f03eb6
gsb-cdi-jmm <39127299+gsb-cdi-jmm@users.noreply.github.com>
2018-05-16 10:16:33 +0200

    Add files via upload

--- recupRAPPORT_VISITE.php
+++ recupRAPPORT_VISITE.php
@@ -6,28 +6,35 @@ $matricule = htmlentities($_SESSION['matricule']);
 
 include("includes/connexionBdd.php");
 
-//
+//creation des variables
+
 if (isset($_POST['RAP_DATEVISITE'])) {
   $dateVisite = htmlentities($_POST['RAP_DATEVISITE']);
 }
 if(isset($_POST['PRA_NUM']))
 {
   $practicien = htmlentities($_POST['PRA_NUM']);
+  //Requete pour récuperer le numero du practicien
+  $reqPractien = "SELECT `praNum` FROM `praticien` WHERE `praNom` = \"$practicien\"";
+  $repPra = $connexion->query($reqPractien);
+  $praNum = $repPra->fetch();
 }
+
+
 if (isset($_POST['RAP_MOTIF'])) {
   $rapMotif = htmlentities($_POST['RAP_MOTIF']);
 }
 
 
-
-var_dump($practicien);
+echo "$matricule";
+echo $praNum['0'];
 
 //On vérifie que le praticien à rentrer toutes les informations correctements
 //On verifie que la date n'est pas vide
 if ($dateVisite != "") {
   //On vérifie que le practicien a été selectionné
   if ($practicien != "") {
-    $req = "INSERT INTO rapportvisite(visMatricule, praNum, rapDate) values('$matricule', 1, '$dateVisite')";
+    $req = "INSERT INTO rapportvisite(visMatricule, praNum, rapDate) values('$matricule', " . $praNum['0'] . ", '$dateVisite')";
     $rep = $connexion->exec($req) or die("Erreur dans la requete");
     echo "$dateVisite";
     echo "test";
